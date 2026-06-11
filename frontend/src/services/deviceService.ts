8aa371e05cefb83fa24e838edee191d4b7124618
Md Kamrul Amin <kamjony7@gmail.com>
2026-06-11 13:36:45 -0400

    Create device service

---
+++ frontend/src/services/deviceService.ts
@@ -0,0 +1,35 @@
+import { apiRequest } from "./apiClient"
+import type { CreateDeviceRequest, CreateDeviceaResponse, Device, } from "../types/device"
+
+//Fetch all devices owned by the logged-in user.
+export function getDevices(token: string) {
+    return apiRequest<Device[]>('/devices', {
+        method: 'GET',
+        token,
+    })
+}
+
+//Fetch one device by its internal db ID.
+export function getDevice(deviceId: number, token: string) {
+    return apiRequest<Device>(`/devices/${deviceId}`, {
+        method: 'GET',
+        token,
+    })
+}
+
+//Create a new device. The backend will reutnr the raw API key once.
+export function createDevice(payload: CreateDeviceRequest, token: string) {
+    return apiRequest<CreateDeviceRequest>('/devices', {
+        method: 'POST',
+        body: payload,
+        token,
+    })
+}
+
+//Delete one device by its internal db ID.
+export function deleteDevice(deviceId: number, token: string) {
+    return apiRequest<void>(`/devices/${deviceId}`, {
+        method: 'DELETE',
+        token,
+    })
+}
